b5f6cf7fc58603fed3f5202cb247cbdca5959130
Kiran Brar <studio@maphive.com.au>
2020-08-20 19:49:28 +1000

    Multiple fields form working

--- src/components/form.js
+++ src/components/form.js
@@ -1,48 +1,83 @@
 import React, { useState } from "react";
 import firebase from "../base";
 
-const Form = () => {
-  const [input, setInput] = useState("");
+// const Form = () => {
+// const [input, setInput] = useState("");
 
-  const handleOnChange = (e) => {
-    setInput(e.target.value);
+// const handleOnChange = (e) => {
+//   setInput(e.target.value);
+// };
+
+// const pushData = () => {
+//   const dataRef = firebase.database().ref("Data");
+//   const data = {
+//     input,
+//   };
+
+//   dataRef.push(data);
+// };
+
+// return (
+//   <div>
+//     <input type="text" onChange={handleOnChange} value={input} />
+//     <button onClick={pushData}>Add Data</button>
+//   </div>
+// );
+
+const Form = () => {
+  const initialFieldValues = {
+    q1: "",
+    q2: "",
   };
 
-  const pushData = () => {
-    const dataRef = firebase.database().ref("Data");
-    const data = {
-      input,
-    };
+  const [values, setValues] = useState(initialFieldValues);
 
-    dataRef.push(data);
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const dataRef = firebase.database().ref("Submission");
+    // const data = { values }; Not needed but kept for reference
+
+    dataRef.push(values);
   };
 
   return (
-    <div>
-      <form>
-        <div>
-          <label>Question 1</label>
-          <input
-            type="email"
-            name="email"
-            id="InputEmail1"
-            placeholder="Enter answer 1"
-            required="required"
-          />
-        </div>
-        <div>
-          <label>Question 2</label>
-          <input
-            type="text"
-            name="name"
-            id="leInputName"
-            placeholder="Enter answer 2"
-            required="required"
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <form>
+      <div>
+        <label>Question 1</label>
+        <input
+          type="text"
+          name="q1"
+          id="q1"
+          placeholder="Enter answer 1"
+          required="required"
+          value={values.q1}
+          onChange={handleOnChange}
+        />
+      </div>
+      <div>
+        <label>Question 2</label>
+        <input
+          type="text"
+          name="q2"
+          id="q2"
+          placeholder="Enter answer 2"
+          required="required"
+          value={values.q2}
+          onChange={handleOnChange}
+        />
+      </div>
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </form>
   );
 };
 
