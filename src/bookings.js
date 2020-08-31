1eb0eede7f42b8af537186c8d8bfc57474040b0a
Kiran Brar <studio@maphive.com.au>
2020-08-31 22:45:49 +1000

    Basic booking push working

--- src/bookings.js
+++ src/bookings.js
@@ -2,6 +2,7 @@ import "date-fns";
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import DateFnsUtils from "@date-io/date-fns";
+import firebase from "./base";
 import { format, formatDistance, formatRelative, subDays } from "date-fns";
 
 // When storing date (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString) { user: …, date: date.toISOString() }
@@ -14,11 +15,20 @@ import {
 export default function MaterialUIPickers() {
   // The first commit of Material-UI
   const [selectedDate, setSelectedDate] = React.useState(
-    new Date("2014-08-18T21:11:54")
+    new Date("2020T12:00:00")
   );
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const bookingRef = firebase.database().ref("Booking");
+    const bookingFormat = format(selectedDate, "'Today is a' iiii");
+    const booking = bookingFormat;
+
+    bookingRef.push(booking);
   };
 
   return (
@@ -29,7 +39,7 @@ export default function MaterialUIPickers() {
           <KeyboardDatePicker
             margin="normal"
             id="date-picker-dialog"
-            label="Date picker dialog"
+            label="Select a date"
             format="MM/dd/yyyy"
             value={selectedDate}
             onChange={handleDateChange}
@@ -41,7 +51,7 @@ export default function MaterialUIPickers() {
             minutesStep={15}
             margin="normal"
             id="time-picker"
-            label="Time picker"
+            label="Select a time"
             value={selectedDate}
             onChange={handleDateChange}
             KeyboardButtonProps={{
@@ -49,7 +59,9 @@ export default function MaterialUIPickers() {
             }}
           />
         </Grid>
-        <button type="submit">Book Gym</button>
+        <button type="submit" onClick={handleSubmit}>
+          Book Gym
+        </button>
       </MuiPickersUtilsProvider>
     </>
   );
